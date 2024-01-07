5bfea833c2bb372134222c42bd529b7fe18a9d11
luischiliguano <95940533+luischiliguano@users.noreply.github.com>
2024-01-07 17:33:27 -0500

    Deleteroute a medias

--- controller/trackController.js
+++ controller/trackController.js
@@ -3,10 +3,29 @@ const controller = {
     getAllRoutes: async (req, res) => {
         try {
             const routes = await Track.findAll();
-            res.status(200).json(routes);
+            res.status(200).send({ routes: routes });
         } catch (error) {
             console.error('Error al obtener las rutas:', error);
-            res.status(500).json({ error: 'Error interno del servidor' });
+            res.status(500).send({ error: 'Error interno del servidor' });
+        }
+    },
+
+    deleteRoute: async (req, res) => {
+        const routeId = req.params.id;
+
+        try {
+            const deletedRoute = await Track.destroy({
+                where: { ID_ROUTE: routeId }
+            });
+
+            if (deletedRoute === 1) {
+                res.status(200).send({ message: 'Ruta eliminada exitosamente' });
+            } else {
+                res.status(404).send({ error: 'Ruta no encontrada' });
+            }
+        } catch (error) {
+            console.error('Error al eliminar la ruta:', error);
+            res.status(500).send({ error: 'Error interno del servidor' });
         }
     }
   
