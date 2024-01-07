b4fb21ef6a17f6d9f1bb5f2d20a184142eebc10f
Kevin Toasa <kevintoasa43@gmail.com>
2024-01-07 17:32:18 -0500

    Agregar el metodo update a los usuarios

--- controller/userController.js
+++ controller/userController.js
@@ -62,7 +62,41 @@ const controller = {
         console.error("Error al iniciar sesión:", error);
         return res.status(500).send({ error: 'Error al iniciar sesión' });
       }
+      },
+      
+      updateUserPassword: async function(req, res) {
+        try {
+          const userId = req.params.id; // Obtener el ID del usuario desde la URL
+          const { password } = req.body;
+    
+          // Encontrar el usuario por su ID
+          const user = await User.findByPk(userId);
+          if (!user) {
+            return res.status(404).send({ msg: 'Usuario no encontrado' });
+          }
+    
+          // Verificar si se proporcionó la nueva contraseña
+          if (!password) {
+            return res.status(400).send({ msg: 'Se requiere una nueva contraseña' });
+          }
+    
+          // Actualizar la contraseña
+          const hashedPassword = await bcrypt.hash(password, 10);
+          user.PASSWORD = hashedPassword;
+    
+          // Guardar la nueva contraseña en la base de datos
+          await user.save();
+    
+          // Enviar respuesta con confirmación de actualización
+          return res.status(200).send({
+            msg: 'Contraseña actualizada exitosamente para el usuario ID: ' + userId
+          });
+        } catch (error) {
+          console.error("Error al actualizar la contraseña:", error);
+          return res.status(500).send({ error: 'Error al actualizar la contraseña' });
+        }
       }
+    
 
 }
 function isTokenValid(token) {
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,4 +3,5 @@ const User = require('../controller/userController');
 const router = express.Router();
 router.post('/createUser', User.newUser);
 router.post('/login', User.loginUser);
+router.put('/update/:id',User.updateUserPassword)
 module.exports = router;
